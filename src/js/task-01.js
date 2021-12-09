b3a3fc65c376a7770477556877ab3520ff600dab
sergiihsv <hsv-serg@ukr.net>
2021-12-09 15:53:20 +0200

    09121553

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,28 +1,10 @@
-/* const getElementItem = document.querySelectorAll('.item');
-
-console.log('Categories: ${getElementItem.length}');
-
-getElementItem.forEach(element => {
-    
-    const getElementTitle = element.querySelector('h2');
-    const getElementLi = element.querySelectorAll('li');
-
-    console.log('Category: ${getElementTitle.textContent}');
-    console.log('Elements: ${getElementLi.length}');
-
-})
- */
-
-const categories = document.querySelector("ul#categories");
-console.log(
-	"Number of categories (НОМЕРА КАТЕГОРИЙ) : ",
-	categories.children.length
+const categoriesEl = document.querySelector("ul#categories");
+console.log( "Number of categories : ", categoriesEl.children.length
 );
-const titleElement = document.querySelectorAll(".item");
-titleElement.forEach((element) => {
-	console.log("Category/Категория: ", element.firstElementChild.textContent);
-	console.log(
-		"Elements/Элементы: ",
-		element.firstElementChild.nextElementSibling.children.length
+
+const titleEl = document.querySelectorAll(".item");
+titleEl.forEach((element) => {
+	console.log("Category:", element.firstElementChild.textContent);
+	console.log("Elements: ", element.firstElementChild.nextElementSibling.children.length
 	);
 });
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,38 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ const ulEl = document.querySelector("#ingredients");
+
+
+const elements = ingredients.map(ingredient => {
+  
+const createLiEl = document.createElement('li');
+createLiEl.textContent = '';
+createLiEl.classList.add('item');
+
+  return createLiEl;
+}
+)
+  
+
+/* elements(ingredients); */
+ulEl.append(...elements);
+
+
+
+/* const createLiEl = document.createElement('li');
+createLiEl.textContent = '';
+createLiEl.classList.add('item'); */
+
+
+
+
+
+
+
+
+console.log(ulEl);
+
+
+
