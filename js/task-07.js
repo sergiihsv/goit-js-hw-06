63d0a6e006b70273ffe1962ccf4627aa5808c4e7
sergiihsv <hsv-serg@ukr.net>
2021-12-12 22:12:48 +0200

    7 task done

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const textInput = document.querySelector('#font-size-control');
+const textChangeSize = document.querySelector('#text');
+
+
+const onSliderTextChange = () => {
+    textChangeSize.style.fontSize = `${textInput.value}` + "px";
+}
+
+
+textInput.addEventListener("input", onSliderTextChange);
