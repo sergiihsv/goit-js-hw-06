335924cc0132f56159cd0fd1f304f72895a82290
sergiihsv <hsv-serg@ukr.net>
2021-12-13 14:12:49 +0200

    9 task  done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const buttonChangeColorEl = document.querySelector('.change-color');
+const spanValueEl = document.querySelector('.color');
+
+ const onBodyBackgroundColor = () => {
+
+  const currentColor = getRandomHexColor();
+  document.body.style.backgroundColor = currentColor;
+  spanValueEl.textContent = currentColor;
+
+} 
+
+
+buttonChangeColorEl.addEventListener('click', onBodyBackgroundColor);
+
+
